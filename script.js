63e80a4c5e565bf8ade160dd951a8c28c083284b
Helena2018 <helena2020liu@gmail.com>
2022-07-09 18:52:47 +1200

    adding search_api

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const API_URL = 'http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=88caa129da14fee33827b881ed88a410&page1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w500'
-const SEARCH_URL = 'http://api.themoviedb.org/3/search/movie?api_key=88caa129da14fee33827b881ed88a410&query="'
+const SEARCH_API  = 'http://api.themoviedb.org/3/search/movie?api_key=88caa129da14fee33827b881ed88a410&query="'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
 
 // Get initial getMovies
 getMovies(API_URL)
@@ -11,3 +14,17 @@ async function getMovies(url) {
 
   console.log(data.results)
 }
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const searchTerm = search.value 
+
+  if(searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_API + searchTerm)
+
+    search.value = ''
+  } else {
+    window.location.reload()
+  }
+})
